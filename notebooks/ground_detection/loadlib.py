60fccd4c9436702bcbea451660a353e1344afd40
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-04-29 20:08:51 +0200

    changed path to rootdir

--- notebooks/ground_detection/loadlib.py
+++ notebooks/ground_detection/loadlib.py
@@ -6,6 +6,6 @@ import os
 import sys
 
 modules = os
-module_path = os.path.abspath(os.path.join('..'))
+module_path = os.path.abspath(os.path.join('../..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
