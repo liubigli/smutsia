a15d24d6d25a779bfa4e2fc1c552d08e5963a929
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-04-29 19:57:29 +0200

    added auxiliary function that print picked values

--- smutsia/utils/viz.py
+++ smutsia/utils/viz.py
@@ -2,8 +2,12 @@ import numpy as np
 import pyvista as pv
 from scipy.sparse import find
 
-def plot_cloud(xyz, scalars=None, cmap=None, point_size=1.0, graph=None):
-
+def plot_cloud(xyz, scalars=None, cmap=None, point_size=1.0, graph=None, interact=False):
+    """
+    Helper functions
+    Parameters
+    ----------
+    """
     plotter = pv.BackgroundPlotter()
     poly = pv.PolyData(xyz)
     plotter.add_mesh(poly, scalars=scalars, cmap=cmap, point_size=point_size)
@@ -21,19 +25,24 @@ def plot_cloud(xyz, scalars=None, cmap=None, point_size=1.0, graph=None):
         # mapper.SetInputData(lines)
         plotter.add_lines(lines, width=1)
 
-    print("TEST FUNC")
-    plotter.enable_cell_picking(through=False, callback=mymessage)
+    # auxiliary function to analyse the picked cells
+    def analyse_picked_points(picked_cells):
+        ids = picked_cells.point_arrays['vtkOriginalPointIds']
+        print("Selected Points: ")
+        print(ids)
+        print("Coordinates xyz: ")
+        print(xyz[ids])
+
+        if scalars is not None:
+            print("Labels: ")
+            print(scalars[ids])
+
+    if interact:
+        plotter.enable_cell_picking(through=False, callback=analyse_picked_points)
+
     return plotter
 
-
-def mymessage(picked_cells):
-    print(picked_cells.point_arrays['vtkOriginalPointIds'])
-    # print(picked_cells, "Hello World")
-
-def analyse_plot(xyz, scalars=None, cmap=None, point_size=1.0, graph=None):
-    plotter = plot_cloud(xyz, scalars=scalars, cmap=cmap, point_size=point_size, graph=graph)
-    # sel_points = None
-
-    # while True:
-    #     if plotter.picked_cells is not None:
-    #         pass
+#
+# def mymessage(picked_cells):
+#     print(picked_cells.point_arrays['vtkOriginalPointIds'])
+#     # print(picked_cells, "Hello World")
