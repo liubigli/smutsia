229a03023f6776a2bbd3fe54dc8fa611869982c1
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-05-14 12:03:14 +0200

    updated links to functions in graph module and in array module

--- smutsia/utils/__init__.py
+++ smutsia/utils/__init__.py
@@ -2,13 +2,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__all__= [
+__all__ = [
     'resize_graph',
     'shuffle_labels',
     'reconstruct_ith_mst',
     'get_positive_degree_nodes',
     'get_subgraph',
     'merge_graphs',
+    'cloud_knn_graph',
+    'cloud_spherical_graph',
+    'cloud_3d_graph',
     'pixel_to_node',
     'node_to_pixel',
     'img_to_graph',
@@ -18,12 +21,15 @@ __all__= [
     'label_image',
     'stick_two_images',
     'cartesian_product',
+    'set_distance',
+    'subset_backprojection',
+    'subset_projection',
     'compute_scores'
 ]
 
-
-from .graph import resize_graph, shuffle_labels, reconstruct_ith_mst, get_positive_degree_nodes, get_subgraph, merge_graphs
+from .graph import resize_graph, shuffle_labels, reconstruct_ith_mst, get_positive_degree_nodes, get_subgraph
+from .graph import merge_graphs, cloud_knn_graph, cloud_spherical_graph, cloud_3d_graph
 from .image import pixel_to_node, node_to_pixel, img_to_graph, plot_graph, plot_sub_graph, accumarray, label_image
 from .image import stick_two_images
-from .arrays import cartesian_product
+from .arrays import cartesian_product, set_distance, subset_backprojection, subset_projection
 from .scores import compute_scores
