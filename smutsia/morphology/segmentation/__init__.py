2fb4c17f8ff3142cf1f8f15cb059c9ac6b640ba2
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-05-16 14:40:56 +0200

    addded z_nz_dist to __all__ list

--- smutsia/morphology/segmentation/__init__.py
+++ smutsia/morphology/segmentation/__init__.py
@@ -6,9 +6,11 @@ __all__ = [
     # 'alpha_omega_constrained_connectivity',
     'quasi_flat_zones',
     'threshold_edges',
+    'z_nz_dist'
 
 ]
 
 # from ._morphological_segmentation import alpha_omega_constrained_connectivity
 from ._morphological_segmentation import quasi_flat_zones
 from ._morphological_segmentation import threshold_edges
+from .metrics import z_nz_dist
