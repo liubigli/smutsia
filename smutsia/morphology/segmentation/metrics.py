d7a6eea8b8742a4fc70d3c8dd4140f8b32a22e32
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-05-16 14:40:37 +0200

    added aggr parameter that allows to compute dist both as max(f(src), f(dst)) or |f(src)-f(dst)|

--- smutsia/morphology/segmentation/metrics.py
+++ smutsia/morphology/segmentation/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def _logistic_function(x, x0, L, k):
     """
     General logistic function. It returns
@@ -16,12 +17,14 @@ def _logistic_function(x, x0, L, k):
     k: float
         power to the exponential function
 
-    Returns:
+    Returns
+    -------
     y: ndarray
     """
     return L / (1 + np.exp(-k * (x - x0)))
 
-def z_nz_dist(xyz, normals, src, dst):
+
+def z_nz_dist(xyz, normals, src, dst, aggr='diff'):
     """
     Custom metric used for Ground segmentation. Basically the weight between two points is the difference of the
     ratios elevation / vertical orientation.
@@ -40,15 +43,23 @@ def z_nz_dist(xyz, normals, src, dst):
     dst: ndarray
         array of destination ids
 
+    aggr: optional {'diff', 'max'}
+        if 'diff' take diff between ratios
+        if 'max' take max between ratios
+
     Returns
     -------
     weights: ndarray
         array of weights
     """
     z = xyz[:, 2] - xyz[:, 2].min()
-    nz = np.abs(normals[:,2])
+    nz = np.abs(normals[:, 2])
 
     # apply logistic function to normals
     log_nz = _logistic_function(nz, x0=0.7, L=1, k=16)
+    if aggr == 'max':
+        weights = np.c_[(z[src] / log_nz[src]), (z[dst] / log_nz[dst])].max(axis=1)
+    else:
+        weights = np.abs((z[src] / log_nz[src]) - (z[dst] / log_nz[dst]))
 
-    return np.abs((z[src] / log_nz[src]) - (z[dst] / log_nz[dst]))
+    return weights
