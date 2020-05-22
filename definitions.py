74cab9e123f50f986076a0b3acac0183a93e9c8f
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-05-22 18:34:45 +0200

    added path to semantic kitti config file

--- definitions.py
+++ definitions.py
@@ -4,6 +4,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # DATA PATH
 DATA_ROOT_PATH = os.path.join(PROJECT_ROOT, 'data')
 SEMANTICKITTI_PATH = os.path.join(DATA_ROOT_PATH, 'semantickitti', 'dataset', 'sequences')
+SEMANTICKITTI_CONFIG = os.path.join(PROJECT_ROOT, 'config', 'labels', 'semantic-kitti.yaml')
 
 # RESULTS PATH
 RESULTS_ROOT_PATH = os.path.join(PROJECT_ROOT, 'results')
