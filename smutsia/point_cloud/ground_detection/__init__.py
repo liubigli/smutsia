85f3e771d3e814a46dbb4cf7969ea8f69c58287e
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-05-22 18:08:31 +0200

    added dart_ground_detection method

--- smutsia/point_cloud/ground_detection/__init__.py
+++ smutsia/point_cloud/ground_detection/__init__.py
@@ -5,8 +5,10 @@ from __future__ import absolute_import
 
 __all__ = [
     'naive_ransac',
-    'hybrid_ground_detection'
+    'hybrid_ground_detection',
+    'dart_ground_detection'
 ]
 
 from ._ransac import naive_ransac
 from ._hybrid import hybrid_ground_detection
+from ._bev_qfz import dart_ground_detection
