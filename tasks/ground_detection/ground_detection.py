5feabf397d4014ea5c779a6876bf3e26d14a8845
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-05-28 10:33:55 +0200

    added function to store point cloud prediction on files and to stock results together.

--- tasks/ground_detection/ground_detection.py
+++ tasks/ground_detection/ground_detection.py
@@ -1,4 +1,5 @@
 import os
+import ntpath
 import argparse
 import numpy as np
 from glob import glob
@@ -7,7 +8,7 @@ from smutsia.utils import process_iterable, load_yaml
 from smutsia.utils.semantickitti import load_pyntcloud, SemanticKittiConfig
 from smutsia.point_cloud.ground_detection import *
 from smutsia.utils.scores import compute_scores, get_confusion_matrix, condense_confusion_matrix
-from smutsia.utils.viz import plot_confusion_matrix
+from smutsia.utils.viz import plot_confusion_matrix, color_bool_labeling
 
 
 carLikeId = [10, 13, 18]
@@ -23,6 +24,32 @@ condensedId = []
 for elem in mySelect:
     selectedId.extend(elem)
     condensedId.append(elem)
+
+
+def write_las(points, file_name, labels=None, color=None):
+    import laspy
+
+    hdr = laspy.header.Header(file_version=1.4, point_format=2)
+
+    outfile = laspy.file.File(file_name, mode="w", header=hdr)
+    min_X, min_Y, min_Z = points.min(0)
+
+    outfile.header.offset = [min_X, min_Y, min_Z]
+    outfile.header.scale = [0.001, 0.001, 0.001]
+
+    outfile.x = points[:, 0]
+    outfile.y = points[:, 1]
+    outfile.z = points[:, 2]
+    # labels = np.ones(len(points))
+    if labels is not None:
+        outfile.user_data = labels
+
+    if color is not None:
+        outfile.red = color[:, 0].astype(np.uint8)
+        outfile.green = color[:, 1].astype(np.uint8)
+        outfile.blue = color[:, 2].astype(np.uint8)
+
+    outfile.close()
 
 
 def recursively_add_params(key, yaml_config, params):
@@ -66,11 +93,13 @@ def load_parameters(config_file):
     for k in yaml_config:
         recursively_add_params(k, yaml_config, params)
 
+    method_name = "all"
     # remove key name
     if 'name' in params:
+        method_name = params['name']
         params.pop('name')
 
-    return params
+    return params, method_name
 
 
 def load_sequence(datapath, start, end, step, extension='bin'):
@@ -104,6 +133,23 @@ def load_sequence(datapath, start, end, step, extension='bin'):
 
     return files[start:end:step]
 
+def aggregate_results(chunk_clouds, savedir, method_name):
+    """
+    Function that aggregates results obtained for each cloud
+    """
+    with open(os.path.join(savedir, method_name + '.txt'), 'a') as f1:
+        for cloud in chunk_clouds:
+            filename = cloud.filename
+            if hasattr(cloud, 'sequence'):
+                filename = cloud.sequence + '_' + filename
+
+            with open(os.path.join(savedir, filename + '.txt'), 'r') as f:
+                lines = f.readlines()
+                for l in lines:
+                    f1.write(l)
+                f.close()
+            os.remove(os.path.join(savedir, filename + '.txt'))
+    f1.close()
 
 def analyse_results(data, savedir, ground_id=40, classes=None):
     """
@@ -124,19 +170,41 @@ def analyse_results(data, savedir, ground_id=40, classes=None):
     y_pred = data[0]
     y_true = data[1]
     labels = data[2]
-    filepath = data[3]
-    filename = filepath.split('/')[-1].split('.')[0]
+    cloud = data[3]
+    if isinstance(cloud, str):
+        filename = cloud
+    else:
+        filename = cloud.filename
+        if hasattr(cloud, 'sequence'):
+            filename = cloud.sequence + '_' + filename
 
     # compute scores
-    scores = compute_scores(y_true, y_pred, print_info=True)
-    f = open(os.path.join(savedir, filename + '.txt'), "w")
-    f.write(str(scores))
+    scores = compute_scores(y_true, y_pred, print_info=True, sample_name=filename)
+    open_type = "w" if not isinstance(cloud, str) else "a"
+    f = open(os.path.join(savedir, filename + '.txt'), open_type)
+    info_scores = "Scores {}: \n" \
+                  "F1 -> {},\n" \
+                  "Recall -> {},\n" \
+                  "Precision -> {},\n" \
+                  "Accuracy -> {},\n" \
+                  "Jaccard -> {}.\n" \
+                  "-----------------------\n".format(filename,
+                                          scores['f1'],
+                                          scores['recall'],
+                                          scores['precision'],
+                                          scores['acc'],
+                                          scores['jaccard'])
+    f.write(info_scores)
     f.close()
 
     cm = get_confusion_matrix(labels, ground_id * y_pred, selectedId)
     cm = condense_confusion_matrix(cm[0], selectedId, condensedId)
     plot_confusion_matrix(cm, classes=classes, normalize=True, savefig=os.path.join(savedir, filename + '.eps'),
                           title='Confusion Matrix ' + filename.upper())
+
+    if not isinstance(cloud, str):
+        color = color_bool_labeling(y_true, y_pred)
+        write_las(cloud.xyz, file_name=os.path.join(savedir, filename + '.las'), color=color)
 
 
 def main(dataset, method, config_file, savedir, sequence, start=0, end=-1, step=1, chunk_size=-1):
@@ -186,7 +254,7 @@ def main(dataset, method, config_file, savedir, sequence, start=0, end=-1, step=
         raise ValueError("Method {} not known.".format(method))
 
     # load model parameters
-    params = load_parameters(config_file)
+    params, method_name = load_parameters(config_file)
 
     # initialise dataset variables
     if dataset == 'semantickitti':
@@ -226,10 +294,10 @@ def main(dataset, method, config_file, savedir, sequence, start=0, end=-1, step=
         chunk_pred = process_iterable(clouds, func, **params)
         print("Analysing predictions for chunk {}-{}".format(i, min(i + chunk_size, n)))
         # analyse and plot results for each file in chunk
-        process_iterable(zip(chunk_pred, chunk_gt, chunk_labels, files[i:i+chunk_size]),
+        process_iterable(zip(chunk_pred, chunk_gt, chunk_labels, clouds),
                          analyse_results,
                          **{'savedir': savedir, 'classes': classes, 'ground_id': ground_id})
-
+        aggregate_results(clouds, savedir, method_name)
         labels += chunk_labels
         y_true += chunk_gt
         y_pred += chunk_pred
@@ -239,7 +307,7 @@ def main(dataset, method, config_file, savedir, sequence, start=0, end=-1, step=
     labels = np.concatenate(labels)
     print("Compute total scores")
     # analyse and plot global results
-    analyse_results([y_pred, y_true, labels, 'all.txt'], savedir=savedir, ground_id=ground_id, classes=classes)
+    analyse_results([y_pred, y_true, labels, method_name], savedir=savedir, ground_id=ground_id, classes=classes)
 
 
 if __name__ == "__main__":
@@ -256,7 +324,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     dataset = args.dataset
     gd_method = args.method
-    params_file = args.params
+    params_path = args.params
     seq = args.sequence
     start_frame = args.start
     end_frame = args.end
@@ -265,14 +333,20 @@ if __name__ == "__main__":
     print("Evaluating performance of method {}, over Dataset {}."
           " Selected sequence {} and interval {}-{} each {}.".format(gd_method, dataset, seq,
                                                                      start_frame, end_frame, step_frames))
+    params_file = ntpath.basename(params_path)
+    out = params_file.split('.')[:-1]
+    if len(out) == 1:
+        params_file = out[0]
+    else:
+        params_file = '.'.join(out)
 
-    savedir = os.path.join('/home/leonardo/Dev/github/smutsia/results/ground_detection/', gd_method)
+    savedir = os.path.join('/home/leonardo/Dev/github/smutsia/results/ground_detection/', gd_method, params_file)
     if not os.path.exists(savedir):
         os.makedirs(savedir)
 
     main(dataset=dataset,
          method=gd_method,
-         config_file=params_file,
+         config_file=params_path,
          savedir=savedir,
          sequence=seq,
          start=start_frame,
