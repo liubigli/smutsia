7b5ed990a50c240d1194847d77e278108374e201
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-06-16 15:26:57 +0200

    changed function that condense labels in confusion matrix

--- smutsia/utils/scores.py
+++ smutsia/utils/scores.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, jaccard_score, confusion_matrix
+from smutsia.utils.arrays import cartesian_product
 
 
 def compute_scores(y_true, y_pred, threshold=0.0, print_info=False, sample_name=None):
@@ -47,17 +48,16 @@ def compute_scores(y_true, y_pred, threshold=0.0, print_info=False, sample_name=
 
     return scores
 
+
 def mat_renorm_rows(M):
     sr = M.astype(float).sum(axis=1)[:, np.newaxis]
-    return np.divide(M, sr, where=sr!=0.0)
+    return np.divide(M, sr, where=sr != 0.0)
+
 
 def get_confusion_matrix(y_true, y_pred, selectedId):
-
-    conf_mat = confusion_matrix(y_true,y_pred, labels=selectedId)
+    conf_mat = confusion_matrix(y_true, y_pred, labels=selectedId)
     conf_mat_norm = mat_renorm_rows(conf_mat)
-
-
-    return(conf_mat,conf_mat_norm)
+    return conf_mat, conf_mat_norm
 
 
 def normalize_confusion_matrix(conf_mat):
@@ -67,42 +67,38 @@ def normalize_confusion_matrix(conf_mat):
 
     for i in range(myLen):
         currentSum = mySum[i]
-        if(currentSum > 0):
+        if currentSum > 0:
             for j in range(myLen):
-                conf_mat_norm[i,j] = conf_mat[i,j] / mySum[i]
+                conf_mat_norm[i, j] = conf_mat[i, j] / mySum[i]
 
     conf_mat_norm = np.around(conf_mat_norm, decimals=2)
 
     return conf_mat_norm
 
-def condense_confusion_matrix(conf_mat, inputID, outputID):
-    tmp_conf_mat = np.zeros(conf_mat.shape)
-    tmp_conf_mat[:,:]=conf_mat[:,:]
-    deleted_index = []
-    for elem in outputID:
-        if(type(elem) != int):
-            first = 1
-            for id in elem:
-                if (not first):
-                    myindex = inputID.index(id)
-                    tmp_conf_mat[:,first_index]=tmp_conf_mat[:,first_index]+tmp_conf_mat[:,myindex]
-                    tmp_conf_mat[first_index,:]=tmp_conf_mat[first_index,:]+tmp_conf_mat[myindex,:]
-                    deleted_index.append(myindex)
-                else:
-                    first_index = inputID.index(id)
-                first = 0
 
-    myshape = conf_mat.shape
+def condense_confusion_matrix(conf_mat, input_labels, condense_list):
+    condensed_mat = np.zeros((len(condense_list), len(condense_list)))
+    ranges = np.arange(len(condense_list))
+    ext_to_condense = []
+    cond = []
+    for condense in condense_list:
+        cond_i = []
+        for el in condense:
+            cond_i.append(input_labels.index(el))
+        cond.append(cond_i)
 
-    new_conf_mat = np.ndarray((len(outputID),len(outputID)))
-    ii = 0
-    for i in range(myshape[0]):
-        jj = 0
-        if not (i in deleted_index):
-            for j in range(myshape[0]):
-                if not(j in deleted_index):
-                    new_conf_mat[ii,jj]=tmp_conf_mat[i,j]
-                    jj = jj + 1
-            ii = ii + 1
+    for i in ranges:
+        for j in ranges:
+            vec0, vec1 = np.array(cond[i]), np.array(cond[j])
+            c = cartesian_product([vec0, vec1])
+            ii = np.repeat(i, len(c))
+            jj = np.repeat(j, len(c))
+            ext_to_condense.append(np.c_[ii, jj, c])
 
-    return(new_conf_mat)
+    ext_to_condense = np.concatenate(ext_to_condense, axis=0)
+    for coords in ext_to_condense:
+        condensed_mat[coords[0], coords[1]] += conf_mat[coords[2], coords[3]]
+
+    assert condensed_mat.sum() == conf_mat.sum()
+
+    return condensed_mat
