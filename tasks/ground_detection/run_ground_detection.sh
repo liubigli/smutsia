a70867d59a3d87895d96189c364bd0cdc3f2829a
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-06-16 19:25:05 +0200

    updated config file for hybrid method

--- tasks/ground_detection/run_ground_detection.sh
+++ tasks/ground_detection/run_ground_detection.sh
@@ -7,7 +7,7 @@ CHUNK_SIZE=20
 PARAMDIR='../../config/ground_detection/'
 
 python ground_detection.py --dataset=$DATASET --method='qfz' --params=$PARAMDIR'lambda_qfz.yaml' --sequence=$SEQUENCE --start=$START --end=$END --step=$STEP --chunk_size=$CHUNK_SIZE
-python ground_detection.py --dataset=$DATASET --method='hybrid' --params=$PARAMDIR'hybrid_eig.yaml' --sequence=$SEQUENCE --start=$START --end=$END --step=$STEP --chunk_size=$CHUNK_SIZE
+python ground_detection.py --dataset=$DATASET --method='hybrid' --params=$PARAMDIR'hybrid.yaml' --sequence=$SEQUENCE --start=$START --end=$END --step=$STEP --chunk_size=$CHUNK_SIZE
 python ground_detection.py --dataset=$DATASET --method='ransac' --params=$PARAMDIR'ransac.yaml' --sequence=$SEQUENCE --start=$START --end=$END --step=$STEP --chunk_size=$CHUNK_SIZE
 python ground_detection.py --dataset=$DATASET --method='csf' --params=$PARAMDIR'csf_avg_all.yaml' --sequence=$SEQUENCE --start=$START --end=$END --step=$STEP --chunk_size=$CHUNK_SIZE
 python ground_detection.py --dataset=$DATASET --method='csf' --params=$PARAMDIR'csf_avg_mask.yaml' --sequence=$SEQUENCE --start=$START --end=$END --step=$STEP --chunk_size=$CHUNK_SIZE
