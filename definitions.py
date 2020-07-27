f3d660d212930c3e00e5b641d86f17c8007d7b59
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-07-27 16:33:47 +0200

    added definitions for logdir, image dataset, modelnet dataset

--- definitions.py
+++ definitions.py
@@ -5,8 +5,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DATA_ROOT_PATH = os.path.join(PROJECT_ROOT, 'data')
 SEMANTICKITTI_PATH = os.path.join(DATA_ROOT_PATH, 'semantickitti', 'dataset', 'sequences')
 SEMANTICKITTI_CONFIG = os.path.join(PROJECT_ROOT, 'config', 'labels', 'semantic-kitti.yaml')
-
+IMAGES_PATH = os.path.join(DATA_ROOT_PATH, 'images',)
+MODEL_NET = os.path.join(DATA_ROOT_PATH, 'modelnet')
 # RESULTS PATH
 RESULTS_ROOT_PATH = os.path.join(PROJECT_ROOT, 'results')
 BEA_RESULTS = os.path.join(RESULTS_ROOT_PATH, 'ground_detection', 'lambda_fz', 'projections')
 
+# DEEP LEARNING LOG DIR
+LOG_DIR = os.path.join(PROJECT_ROOT, 'logs')
+GROUND_DETECTION_LOG_DIR = os.path.join('ground_detection')
