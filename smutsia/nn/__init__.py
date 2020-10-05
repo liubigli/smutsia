d3c3725a64f8ebfe5933dcc7576ded9c827c8ce0
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-10-05 10:45:03 +0200

    added bias parameter; changed ReLU with LeakyReLU and added negative_slope parameter

--- smutsia/nn/__init__.py
+++ smutsia/nn/__init__.py
@@ -1,7 +1,7 @@
-from torch.nn import Linear, Sequential as Seq, BatchNorm1d, ReLU
+from torch.nn import Linear, Sequential as Seq, BatchNorm1d, LeakyReLU
 
-def MLP(channels):
+def MLP(channels, bias=True, negative_slope=0.0):
     return Seq(*[
-        Seq(Linear(channels[i - 1], channels[i]), BatchNorm1d(channels[i]), ReLU())
+        Seq(Linear(channels[i - 1], channels[i], bias=bias), BatchNorm1d(channels[i]), LeakyReLU(negative_slope=negative_slope))
         for i in range(1, len(channels))
     ])
