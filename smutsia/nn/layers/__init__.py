2a125a449920930065739afc84935149a4881ad7
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-10-14 18:12:52 +0200

    added BilateralConv to init

--- smutsia/nn/layers/__init__.py
+++ smutsia/nn/layers/__init__.py
@@ -6,8 +6,10 @@ __all__ = [
     'DilateMaxPlus',
     'DilateEdgeConv',
     'ErodeEdgeConv',
-    'ErodeFlateEdgeConv'
+    'ErodeFlateEdgeConv',
+    'BilateralConv'
 ]
 
 from .mat_conv import MATConv
 from .morpho_layers import Delirium, DilateFlatEdgeConv, DilateMaxPlus, DilateEdgeConv, ErodeEdgeConv, ErodeFlateEdgeConv
+from ._anisotropic_conv import BilateralConv
