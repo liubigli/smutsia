b431f673c63b591d394f370951eed16987c5860f
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2020-12-01 22:52:52 +0100

    customized version of DynamicEdgeConv that compute cosine distance to find k-neighbors

--- smutsia/nn/conv/__init__.py
+++ smutsia/nn/conv/__init__.py
@@ -7,9 +7,11 @@ __all__ = [
     'DilateEdgeConv',
     'ErodeEdgeConv',
     'ErodeFlateEdgeConv',
-    'BilateralConv'
+    'BilateralConv',
+    'DynamicEdgeConv',
 ]
 
 from .mat_conv import MATConv
 from .morpho_layers import Delirium, DilateFlatEdgeConv, DilateMaxPlus, DilateEdgeConv, ErodeEdgeConv, ErodeFlateEdgeConv
 from ._anisotropic_conv import BilateralConv
+from .edge_conv import DynamicEdgeConv
