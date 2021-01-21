0c742c7833be2409d6b99d0abf6bf4e5bbc13b17
leonardo.gigli <leonardo.gigli@mines-paristech.fr>
2021-01-21 05:30:23 +0100

    added auxiliary functions from Chierchia et Perret

--- smutsia/utils/scores.py
+++ smutsia/utils/scores.py
@@ -1,7 +1,11 @@
 import numpy as np
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, jaccard_score, confusion_matrix
 from smutsia.utils.arrays import cartesian_product
+from scipy.cluster.hierarchy import fcluster
+from sklearn.metrics.cluster import normalized_mutual_info_score as nmi
+from sklearn.metrics.cluster import adjusted_rand_score as ri
 
+import itertools
 
 def compute_scores(y_true, y_pred, threshold=0.0, print_info=False, sample_name=None):
     """Auxiliary Function that compute binary prediction scores.
@@ -102,3 +106,98 @@ def condense_confusion_matrix(conf_mat, input_labels, condense_list):
     assert condensed_mat.sum() == conf_mat.sum()
 
     return condensed_mat
+
+
+############################################################################
+# Copyright ESIEE Paris (2019)                                             #
+#                                                                          #
+# Contributor(s) : Giovanni Chierchia, Benjamin Perret                     #
+#                                                                          #
+# Distributed under the terms of the CECILL-B License.                     #
+#                                                                          #
+# The full license is in the file LICENSE, distributed with this software. #
+############################################################################
+
+
+def get_optimal_k(y, linkage_matrix):
+    best_ri = 0.0
+    n_clusters = y.max() + 1
+    # min_num_clusters = max(n_clusters - 1, 1)
+    best_k = 0
+    best_pred = None
+    for k in range(1, n_clusters + 5):
+        # print(k)
+        y_pred = fcluster(linkage_matrix, k, criterion='maxclust') - 1
+        k_ri = ri(y, y_pred)
+        if k_ri > best_ri:
+            best_ri = k_ri
+            best_k = k
+            best_pred = y_pred
+
+    return best_pred, best_k, best_ri
+
+
+def accuracy_clustering(y_true, y_pred):
+    # Ordering labels
+    labels = np.unique(y_true)
+    ordered_labels = np.arange(labels.shape[0])
+    for k in range(labels.shape[0]):
+        y_true[y_true == labels[k]] = ordered_labels[k]
+    labels = np.unique(y_true)
+
+    scores = []
+
+    # Try all the possible permutations
+    permutations = list(itertools.permutations(labels))
+    for perm in permutations:
+        y_permuted = np.zeros_like(y_true)
+        for i, k in enumerate(perm):
+            y_permuted[y_pred == k] = labels[i]
+        score = accuracy_score(y_true, y_permuted)
+        scores.append(score)
+
+    return max(scores)
+
+
+def purity(y_true, y_pred):
+    # vector which will hold the majority-voted labels
+    y_voted_labels = np.zeros(y_true.shape)
+    # Ordering labels
+    ## Labels might be missing e.g with set like 0,2 where 1 is missing
+    ## First find the unique labels, then map the labels to an ordered set
+    ## 0,2 should become 0,1
+    labels = np.unique(y_true)
+    ordered_labels = np.arange(labels.shape[0])
+    for k in range(labels.shape[0]):
+        y_true[y_true == labels[k]] = ordered_labels[k]
+    # Update unique labels
+    labels = np.unique(y_true)
+    # We set the number of bins to be n_classes+2 so that
+    # we count the actual occurence of classes between two consecutive bins
+    # the bigger being excluded [bin_i, bin_i+1[
+    bins = np.concatenate((labels, [np.max(labels) + 1]), axis=0)
+
+    for cluster in np.unique(y_pred):
+        hist, _ = np.histogram(y_true[y_pred == cluster], bins=bins)
+        # Find the most present label in the cluster
+        winner = np.argmax(hist)
+        y_voted_labels[y_pred == cluster] = winner
+
+    return accuracy_score(y_true, y_voted_labels)
+
+
+def eval_clustering(y_true, Z):
+    n_clusters = y_true.max() + 1
+    y_pred = fcluster(Z, n_clusters, criterion='maxclust') - 1
+    _, y_true = np.unique(y_true, return_inverse=True)
+    _, y_pred = np.unique(y_pred, return_inverse=True)
+
+    # acc_score = accuracy_clustering(y_true, y_pred)
+    pu_score = purity(y_true, y_pred)
+    nmi_score = nmi(y_true, y_pred, average_method='geometric')  # average_method='arithmetic'
+    ri_score = ri(y_true, y_pred)
+    # best_pred, best_k, best_ri = get_optimal_k(y_true, Z)
+    # return acc_score, pu_score, nmi_score, ri_score
+    return pu_score, nmi_score, ri_score
+
+
